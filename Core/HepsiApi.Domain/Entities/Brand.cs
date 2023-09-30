f359ba11c0f677a83c1333695bbcceda9a7ccb3d
burcuustael <burcustael@gmail.com>
2023-09-30 16:18:09 +0300

    temel entityler oluşturuldu.

---
+++ Core/HepsiApi.Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using HepsiApi.Domain.Common;
+
+namespace HepsiApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand() { 
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name { get; set; }
+    }
+}
