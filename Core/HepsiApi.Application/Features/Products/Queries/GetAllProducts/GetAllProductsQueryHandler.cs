df3feef1c6c51553905d42a6e26c580001261271
burcuustael <burcustael@gmail.com>
2023-10-02 21:35:56 +0300

    CQRS e başlandı.

---
+++ Core/HepsiApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace HepsiApi.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
+    {
+        
+        public Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Core/HepsiApi.Domain/Entities/Product.cs
+++ Core/HepsiApi.Domain/Entities/Product.cs
@@ -1,9 +1,4 @@
 ﻿using HepsiApi.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HepsiApi.Domain.Entities
 {
